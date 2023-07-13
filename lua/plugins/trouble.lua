14c9d9df37c229e36943673b93ccc22300391c46
Daniel Cardona Rojas <daniel.cardona@ymedialabs.com>
2023-07-13 13:52:36 -0500

    update trouble bindings

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,7 @@
+return {
+  "folke/trouble.nvim",
+  keys = {
+    { "<leader>x", "<cmd>TroubleToggle workspace_diagnostics<cr>", "Workspace diagnostics" },
+    { "<leader>X", "<cmd>TroubleToggle document_diagnostics<cr>", "Document diagnostics" },
+  },
+}
