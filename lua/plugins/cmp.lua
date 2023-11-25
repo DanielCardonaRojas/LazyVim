63382d41261bdc5621015416964e29644cb8e66c
Daniel Cardona Rojas <d.cardona.rojas@gmail.com>
2023-11-25 16:00:08 -0500

    Disable doc preview for cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -20,6 +20,12 @@ return {
       },
     }
 
+    opts.view = {
+      docs = {
+        auto_open = false,
+      },
+    }
+
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
       ["<C-j>"] = cmp.mapping.select_next_item(),
       ["<C-k>"] = cmp.mapping.select_prev_item(),
